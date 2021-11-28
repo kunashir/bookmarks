f479be453e8186b825741300d5fc99ee8cbd5e2b
Aleksandr <kunash123@gmail.com>
2021-11-28 16:29:23 +0100

    Model Bookmark

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
+  factory :bookmark do
+    title { "F1 forum" }
+    url { "https://reddit.com/formula1" }
+    shortening { "https://f1.x" }
+    site
+  end
+
   factory :site do
     url { "https://reddit.com"  }
   end
